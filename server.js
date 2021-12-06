76a9c68d22e544d898b4759fcb70b7c75a197892
Pedro <pedro_rique17@hotmail.com>
2021-12-06 14:36:58 -0300

    Commit #2

--- server.js
+++ server.js
@@ -48,6 +48,49 @@ app.get('/noticia', async (req, res) => {
 
 });
 
+app.get('/noticia/:id', async(req, res) => {
+
+    var id = parseInt(req.params.id);
+
+    await storage.init();
+
+    const noticias = await storage.getItem("noticias");
+    //console.log(noticias);
+
+    if(noticias === undefined) res.send({error: "Lista de notícias vazia!"});
+
+    else{
+        let noticia = noticias.find(noticia => noticia.id === id);
+  
+        res.send(noticia);
+    }
+   
+});
+
+app.post('/inscricao', async (req, res) => {
+
+    var emails;
+
+    await storage.init();
+
+    emails = await storage.getItem("emails");
+
+    if(emails === undefined){
+        storage.setItem('emails', []);
+    }
+
+    emails = await storage.getItem("emails");
+
+    const email = req.body.email;
+
+    emails.push(email);
+
+   await storage.updateItem('emails', emails);
+
+    res.send("Inscrição com sucesso!");
+
+});
+
 app.listen(3000, () => {
     console.log(`Servidor rodando em http://localhost:3000`);
 })
