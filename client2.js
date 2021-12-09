e5dc2d2c8cf3db07086cf5645f3722541173c923
Pedro <pedro_rique17@hotmail.com>
2021-12-08 23:07:36 -0300

    Commit #5

---
+++ client2.js
@@ -0,0 +1,33 @@
+const axios = require('axios').default;
+const sendEmail = require('./sendEmail');
+
+axios.get('http://localhost:3000/noticia').then(res => {
+
+    const noticias = res.data;
+
+    noticias.forEach(noticia => {
+        console.log("ID: " + noticia.id);
+        console.log("Título: " + noticia.titulo);
+        console.log("Resumo: " + noticia.resumo);
+        console.log("Url: " + noticia.url);
+        console.log("\n");
+    })
+
+    const noticia = noticias[noticias.length-1]; 
+    // Escolhi a última notícia. 
+    //Foi pedido na avaliação que escolhesse uma das notícias para disparar os e-mails
+
+    axios.put('http://localhost:3000/enviar/' + noticia.id).then(res =>{
+        console.log("Notícia de ID " + noticia.id + " enviada!");
+    })
+
+    // Isso mostra todas as notícias
+
+    // noticias.forEach(noticia => {
+    //     console.log("ID: " + noticia.id);
+    //     console.log("Título: " + noticia.titulo);
+    //     console.log("Resumo: " + noticia.resumo);
+    //     console.log("Url: " + noticia.url);
+    //     console.log("\n");
+    // })
+});
--- sendEmail.js
+++ sendEmail.js
@@ -4,15 +4,15 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
     auth: {
-        user: 'blanche.farrell98@ethereal.email',
-        pass: '6v1VcBNyauBpmRV5mw'
+        user: 'maxwell.roob56@ethereal.email',
+        pass: 'UnK4Xph8E9BPNsYXvk'
     }
 });
 
 async function sendEmail(noticia, email){
     try{
         const info = await transporter.sendMail({
-            from: '"Blanche Farrell" <blanche.farrell98@ethereal.email>',
+            from: '"Maxwell Roob" <maxwell.roob56@ethereal.email>',
             to: email,
             subject: noticia.titulo,
             text: noticia.resumo
