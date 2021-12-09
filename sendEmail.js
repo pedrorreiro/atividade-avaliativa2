8d8a38df75025686470c3f4e55c7a5a4554e6e7f
Pedro <pedro_rique17@hotmail.com>
2021-12-09 10:48:07 -0300

    to test

--- sendEmail.js
+++ sendEmail.js
@@ -4,15 +4,15 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
     auth: {
-        user: 'maxwell.roob56@ethereal.email',
-        pass: 'UnK4Xph8E9BPNsYXvk'
+        user: 'sophie.dietrich65@ethereal.email',
+        pass: 'FPRxQUTZKTrhHmshwW'
     }
 });
 
 async function sendEmail(noticia, email){
     try{
         const info = await transporter.sendMail({
-            from: '"Maxwell Roob" <maxwell.roob56@ethereal.email>',
+            from: '"Sophie Dietrich" <sophie.dietrich65@ethereal.email>',
             to: email,
             subject: noticia.titulo,
             text: noticia.resumo
